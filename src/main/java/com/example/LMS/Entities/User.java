984f46a8f3b3c2a866322ae19d6d6874d2d8fb11
Prathamesh <psvagare@gmail.com>
2024-07-09 16:05:18 +0530

    added entities

---
+++ src/main/java/com/example/LMS/Entities/User.java
@@ -0,0 +1,15 @@
+package com.example.LMS.Entities;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Entity
+public class User{
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+}
